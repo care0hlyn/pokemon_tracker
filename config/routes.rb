9ddcba1f09f4f6192c88bd30bf66b7337eb42814
Carolyn Huynh <ckhuynh20@gmail.com>
2014-09-05 11:58:21 -0700

    Create Trainer class and CRUD functionality

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,13 @@ Rails.application.routes.draw do
   match('/animals/:id', {:via => :delete, :to => 'animals#delete'})
   match('/animals/:id/edit', {:via => :get, :to => 'animals#edit'})
   match('/animals/:id', {:via => [:patch, :put], :to => 'animals#update'})
+
+  match('/', {:via => :get, :to => 'trainers#index'})
+  match('/trainers', {:via => :get, :to => 'trainers#index'})
+  match('/trainers/:id/show', {:via => :get, :to => 'trainers#show'})
+  match('/trainers/new', {:via => :get, :to => 'trainers#new'})
+  match('/trainers', {:via => :post, :to => 'trainers#create'})
+  match('/trainers/:id', {:via => :delete, :to => 'trainers#delete'})
+  match('/trainers/:id/edit', {:via => :get, :to => 'trainers#edit'})
+  match('/trainers/:id', {:via => [:patch, :put], :to => 'trainers#update'})
 end
