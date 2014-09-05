492d3229f89ec24e98f8f0d9ce9a5e81f0e9d73f
Carolyn Huynh <ckhuynh20@gmail.com>
2014-09-04 20:48:39 -0700

    Spec passes for Sighting to Animal association - hooray

--- spec/models/sighting_spec.rb
+++ spec/models/sighting_spec.rb
@@ -4,5 +4,5 @@ describe Sighting do
 	it { should validate_presence_of :region }
 	it { should validate_numericality_of :animal_id }
 	it { should validate_numericality_of :trainer_id }
-	it { belongs_to :animal }
+	it { should belong_to :animal }
 end
